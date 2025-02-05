094938a18d46fec51fdc95a049585c19720ff9a5
SurceBeats <subcero@gmail.com>
2025-02-05 16:23:51 +0100

    Oops endless loop

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,13 +1,21 @@
 // U shoulnd't b looking at this file bro lmao
 
 export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  if (url.pathname !== "/") {
+    return context.next();
+  }
+
   const userAgent = context.request.headers.get("User-Agent") || "";
 
   const cliUserAgents = ["curl", "wget", "httpie", "fetch", "http-client", "aria2c"];
 
-  const isCLI = cliUserAgents.some((cli) => userAgent.toLowerCase().includes(cli));
+  const isCLI = cliUserAgents.some(cli => userAgent.toLowerCase().includes(cli));
 
-  const destination = isCLI ? "https://get.homedock.cloud/homedock-os/install.sh" : "https://www.homedock.cloud/install";
+  const destination = isCLI
+    ? "https://get.homedock.cloud/homedock-os/install.sh"
+    : "https://www.homedock.cloud/install";
 
   return Response.redirect(destination, 307);
 }
